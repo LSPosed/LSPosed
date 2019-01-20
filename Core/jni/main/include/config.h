117d3b2ecd31709289a1f94331bb32d6bff9ad3e
solohsu <hsushulong@gmail.com>
2019-01-21 00:58:09 +0800

    Remove debug dex path

--- Core/jni/main/include/config.h
+++ Core/jni/main/include/config.h
@@ -7,13 +7,8 @@
 //#define LOG_DISABLED
 //#define DEBUG
 
-#ifdef DEBUG
-#define INJECT_DEX_PATH \
-"/data/local/tmp/edxposed.dex:/data/local/tmp/eddalvikdx.dex:/data/local/tmp/eddexmaker.dex"
-#else
 #define INJECT_DEX_PATH \
 "/system/framework/edxposed.dex:/system/framework/eddalvikdx.dex:/system/framework/eddexmaker.dex"
-#endif
 
 #define ENTRY_CLASS_NAME "com.elderdrivers.riru.xposed.Main"
 
