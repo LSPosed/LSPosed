b13299ab1a388b5f65cfe060886d1024af988415
solohsu <hsushulong@gmail.com>
2019-03-06 18:12:48 +0800

    v0.3.0.0_beta2

--- Core/template_override/common/post-fs-data.sh
+++ Core/template_override/common/post-fs-data.sh
@@ -7,7 +7,7 @@ MODDIR=${0%/*}
 # More info in the main Magisk thread
 
 # EdXposed Version
-edxp_ver="0.3.0.0_beta1"
+edxp_ver="0.3.0.0_beta2"
 
 # necessary for using mmap in system_server process
 supolicy --live "allow system_server system_server process {execmem}"
