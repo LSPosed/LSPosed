55333f1f47cfaaa975582e578de82698b9cbfc27
solohsu <hsushulong@gmail.com>
2019-05-17 00:29:58 +0800

    Keep Riru module misc files for convenience Prevent libriru_edxp.so not being loaded caused by edxp dir being deleted when switching between different variants (in one case where you firstly install a new variant and then uninstall old one in the Magisk Manager).

--- edxp-core/template_override/uninstall.sh
+++ edxp-core/template_override/uninstall.sh
@@ -1,4 +1,4 @@
 #!/sbin/sh
 
-rm -r -f /data/misc/riru/modules/edxposed/
-rm -r -f /data/misc/riru/modules/edxp/
+#rm -r -f /data/misc/riru/modules/edxposed/
+#rm -r -f /data/misc/riru/modules/edxp/
