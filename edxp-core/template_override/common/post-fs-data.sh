dd042478d765d7e89caee1be100863f5367df0a9
solohsu <hsushulong@gmail.com>
2019-10-19 21:22:11 +0800

    Remove supolicy commands, use magisk-custom-sepolicy-installer instead

--- edxp-core/template_override/common/post-fs-data.sh
+++ edxp-core/template_override/common/post-fs-data.sh
@@ -7,18 +7,18 @@ MODDIR=${0%/*}
 # More info in the main Magisk thread
 
 # necessary for using mmap in system_server process
-supolicy --live "allow system_server system_server process {execmem}"
+#supolicy --live "allow system_server system_server process {execmem}"
 # supolicy --live "allow system_server system_server memprotect {mmap_zero}"
 
 # for built-in apps // TODO maybe narrow down the target classes
-supolicy --live "allow coredomain coredomain process {execmem}"
+#supolicy --live "allow coredomain coredomain process {execmem}"
 
 # read configs set in our app
-supolicy --live "allow coredomain app_data_file * *"
-supolicy --live "attradd {system_app platform_app} mlstrustedsubject"
+#supolicy --live "allow coredomain app_data_file * *"
+#supolicy --live "attradd {system_app platform_app} mlstrustedsubject"
 
 # read module apk file in zygote
-supolicy --live "allow zygote apk_data_file * *"
+#supolicy --live "allow zygote apk_data_file * *"
 
 . ${MODDIR}/util_functions.sh
 
