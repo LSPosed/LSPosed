6537f38309b065d9ecb5f5afbd89112a323a9870
Jim Wu <mlgmxyysd@meowcat.org>
2020-02-26 16:22:02 +0800

    Update stub Manager app

--- edxp-core/template_override/post-fs-data.sh
+++ edxp-core/template_override/post-fs-data.sh
@@ -144,6 +144,11 @@ mkdir "${MODDIR}/system/bin"
 cp -f "/system/bin/app_process32" "${MODDIR}/system/bin/app_process32"
 [[ -f "/system/bin/app_process64" ]] && cp -f "/system/bin/app_process64" "${MODDIR}/system/bin/app_process64"
 
+# install stub if manager not installed
+if [[ "$(pm path org.meowcat.edxposed.manager)" == "" ]]; then
+    pm install ${MODPATH}/EdXposed.apk 2>&2
+fi
+
 start_verbose_log_catcher
 start_bridge_log_catcher
 
