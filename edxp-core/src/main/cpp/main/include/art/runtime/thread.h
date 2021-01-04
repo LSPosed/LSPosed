60a1e52f1080bcd723c6c43706a58a8586374a68
kotori0 <admin@0x.mk>
2021-01-04 18:13:41 +0800

    Fix DecodeJObject crash on x86 and close #783

--- edxp-core/src/main/cpp/main/include/art/runtime/thread.h
+++ edxp-core/src/main/cpp/main/include/art/runtime/thread.h
@@ -7,13 +7,28 @@ namespace art {
 
     class Thread : public edxp::HookedObject {
 
-        CREATE_FUNC_SYMBOL_ENTRY(void *, DecodeJObject, void *thiz,
-                                 jobject obj) {
+#ifdef __i386__
+        typedef void (*DecodeJObjectType)(void **, void *thiz, jobject obj);
+        inline static void (*DecodeJObjectSym)(void **, void *thiz, jobject obj);
+        static void *DecodeJObject(void *thiz, jobject obj) {
+            if (LIKELY(DecodeJObjectSym)) {
+                // Special call conversion
+                void *ret = nullptr;
+                DecodeJObjectSym(&ret, thiz, obj);
+                // Stack unbalanced since we faked return value as 1st param
+                __asm__("sub $0x4, %esp");
+                return ret;
+            } else
+                return nullptr;
+        }
+#else
+        CREATE_FUNC_SYMBOL_ENTRY(void *, DecodeJObject, void *thiz, jobject obj) {
             if (DecodeJObjectSym)
                 return DecodeJObjectSym(thiz, obj);
             else
                 return nullptr;
         }
+#endif
 
     public:
         Thread(void *thiz) : HookedObject(thiz) {}
