2dc7141a38077b97b7272edfaf7a9c919266d8b8
kotori0 <admin@0x.mk>
2021-02-04 02:53:18 +0800

    [core] do not export xhook

--- core/src/main/cpp/external/xhook/src/xhook.h
+++ core/src/main/cpp/external/xhook/src/xhook.h
@@ -28,20 +28,18 @@
 extern "C" {
 #endif
 
-#define XHOOK_EXPORT __attribute__((visibility("default")))
-
 int xhook_register(const char *pathname_regex_str, const char *symbol,
-                   void *new_func, void **old_func) XHOOK_EXPORT;
+                   void *new_func, void **old_func);
 
-int xhook_ignore(const char *pathname_regex_str, const char *symbol) XHOOK_EXPORT;
+int xhook_ignore(const char *pathname_regex_str, const char *symbol);
 
-int xhook_refresh(int async) XHOOK_EXPORT;
+int xhook_refresh(int async);
 
-void xhook_clear() XHOOK_EXPORT;
+void xhook_clear();
 
-void xhook_enable_debug(int flag) XHOOK_EXPORT;
+void xhook_enable_debug(int flag);
 
-void xhook_enable_sigsegv_protection(int flag) XHOOK_EXPORT;
+void xhook_enable_sigsegv_protection(int flag);
 
 #ifdef __cplusplus
 }
