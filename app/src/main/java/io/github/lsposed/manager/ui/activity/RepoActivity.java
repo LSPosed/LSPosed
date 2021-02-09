82729a80ba4d4de6c0dea8ecbd856944626917a5
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-02-09 19:53:03 +0800

    [app] Add module repo

---
+++ app/src/main/java/io/github/lsposed/manager/ui/activity/RepoActivity.java
@@ -0,0 +1,152 @@
+/*
+ * This file is part of LSPosed.
+ *
+ * LSPosed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LSPosed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LSPosed.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2020 EdXposed Contributors
+ * Copyright (C) 2021 LSPosed Contributors
+ */
+
+package io.github.lsposed.manager.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import io.github.lsposed.manager.App;
+import io.github.lsposed.manager.R;
+import io.github.lsposed.manager.databinding.ActivityAppListBinding;
+import io.github.lsposed.manager.repo.RepoLoader;
+import io.github.lsposed.manager.repo.model.OnlineModule;
+import io.github.lsposed.manager.util.LinearLayoutManagerFix;
+import me.zhanghai.android.fastscroll.FastScrollerBuilder;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class RepoActivity extends BaseActivity implements RepoLoader.Listener {
+    private ActivityAppListBinding binding;
+    private RepoAdapter adapter;
+    private RepoLoader repoLoader = RepoLoader.getInstance();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAppListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setNavigationOnClickListener(view -> onBackPressed());
+        binding.masterSwitch.setVisibility(View.GONE);
+        ActionBar bar = getSupportActionBar();
+        assert bar != null;
+        bar.setDisplayHomeAsUpEnabled(true);
+        adapter = new RepoAdapter();
+        binding.recyclerView.setAdapter(adapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManagerFix(this));
+        setupRecyclerViewInsets(binding.recyclerView, binding.getRoot());
+        FastScrollerBuilder fastScrollerBuilder = new FastScrollerBuilder(binding.recyclerView);
+        if (!preferences.getBoolean("md2", true)) {
+            DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,
+                    DividerItemDecoration.VERTICAL);
+            binding.recyclerView.addItemDecoration(dividerItemDecoration);
+        } else {
+            fastScrollerBuilder.useMd2Style();
+        }
+        repoLoader.addListener(this);
+        fastScrollerBuilder.build();
+        binding.swipeRefreshLayout.setOnRefreshListener(() -> {
+            repoLoader.loadRemoteData();
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adapter.setData(repoLoader.getOnlineModules());
+    }
+
+    @Override
+    public void repoLoaded() {
+        runOnUiThread(() -> {
+            binding.swipeRefreshLayout.setRefreshing(false);
+            adapter.setData(repoLoader.getOnlineModules());
+        });
+    }
+
+    private class RepoAdapter extends RecyclerView.Adapter<RepoAdapter.ViewHolder> {
+        private OnlineModule[] modules = new OnlineModule[0];
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_onlinemodule, parent, false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            holder.appName.setText(modules[position].getDescription());
+            String summary = modules[position].getSummary();
+            if (summary != null) {
+                holder.appDescription.setText(modules[position].getSummary());
+            } else {
+                holder.appDescription.setVisibility(View.GONE);
+            }
+            holder.itemView.setOnClickListener(v -> {
+                Intent intent = new Intent();
+                intent.setClass(RepoActivity.this, RepoItemActivity.class);
+                intent.putExtra("module", (Parcelable) modules[position]);
+                startActivity(intent);
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return modules.length;
+        }
+
+        public void setData(OnlineModule[] modules) {
+            this.modules = modules;
+            notifyDataSetChanged();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            View root;
+            TextView appName;
+            TextView appDescription;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                root = itemView.findViewById(R.id.item_root);
+                appName = itemView.findViewById(R.id.app_name);
+                appDescription = itemView.findViewById(R.id.description);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/io/github/lsposed/manager/ui/activity/RepoItemActivity.java
@@ -0,0 +1,219 @@
+/*
+ * This file is part of LSPosed.
+ *
+ * LSPosed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LSPosed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LSPosed.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2020 EdXposed Contributors
+ * Copyright (C) 2021 LSPosed Contributors
+ */
+
+package io.github.lsposed.manager.ui.activity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.github.lsposed.manager.R;
+import io.github.lsposed.manager.databinding.ActivityModuleDetailBinding;
+import io.github.lsposed.manager.databinding.ItemRepoReadmeBinding;
+import io.github.lsposed.manager.databinding.ItemRepoReleaseBinding;
+import io.github.lsposed.manager.databinding.ItemRepoReleasesBinding;
+import io.github.lsposed.manager.repo.model.OnlineModule;
+import io.github.lsposed.manager.repo.model.Release;
+import io.github.lsposed.manager.util.LinearLayoutManagerFix;
+import io.github.lsposed.manager.util.NavUtil;
+import io.noties.markwon.Markwon;
+
+public class RepoItemActivity extends BaseActivity {
+    ActivityModuleDetailBinding binding;
+    private Markwon markwon;
+    private OnlineModule module;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityModuleDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        module = getIntent().getParcelableExtra("module");
+        binding.toolbar.setNavigationOnClickListener(view -> onBackPressed());
+        ActionBar bar = getSupportActionBar();
+        assert bar != null;
+        bar.setTitle(module.getDescription());
+        bar.setSubtitle(module.getName());
+        bar.setDisplayHomeAsUpEnabled(true);
+        markwon = Markwon.create(this);
+        binding.viewPager.setAdapter(new PagerAdapter());
+        binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 0) {
+                    binding.appBar.setLiftOnScrollTargetViewId(R.id.scrollView);
+                } else {
+                    binding.appBar.setLiftOnScrollTargetViewId(R.id.recyclerView);
+                }
+            }
+        });
+        int[] titles = new int[]{R.string.module_readme, R.string.module_readme};
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager, (tab, position) -> tab.setText(titles[position])).attach();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+            ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
+                Insets insets1 = insets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.ime());
+                v.setPadding(insets1.left, insets1.top, insets1.right, 0);
+                return insets;
+            });
+        }
+    }
+
+    private class ReleaseAdapter extends RecyclerView.Adapter<ReleaseAdapter.ViewHolder> {
+        private final List<Release> items;
+
+        public ReleaseAdapter(List<Release> items) {
+            this.items = items;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new ViewHolder(ItemRepoReleaseBinding.inflate(getLayoutInflater(), parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            Release release = items.get(position);
+            holder.title.setText(release.getName());
+            holder.description.setText(release.getDescription());
+            holder.itemView.setOnClickListener(v -> {
+                ArrayList<String> names = new ArrayList<>();
+                release.getReleaseAssets().forEach(releaseAsset -> names.add(releaseAsset.getName()));
+                new MaterialAlertDialogBuilder(RepoItemActivity.this)
+                        .setItems(names.toArray(new String[0]), (dialog, which) -> NavUtil.startURL(RepoItemActivity.this, release.getReleaseAssets().get(which).getDownloadUrl()))
+                        .show();
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView title;
+            TextView description;
+
+
+            public ViewHolder(ItemRepoReleaseBinding binding) {
+                super(binding.getRoot());
+                title = binding.appName;
+                description = binding.description;
+            }
+        }
+    }
+
+    private class PagerAdapter extends RecyclerView.Adapter<PagerAdapter.ViewHolder> {
+
+        @NonNull
+        @Override
+        public PagerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            if (viewType == 0) {
+                return new ViewHolder(ItemRepoReadmeBinding.inflate(getLayoutInflater(), parent, false).getRoot(), viewType);
+            } else {
+                return new ViewHolder(ItemRepoReleasesBinding.inflate(getLayoutInflater(), parent, false).getRoot(), viewType);
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                ViewCompat.setOnApplyWindowInsetsListener(holder.itemView, (v, insets) -> {
+                    Insets insets1 = insets.getInsets(WindowInsetsCompat.Type.systemBars() | WindowInsetsCompat.Type.ime());
+                    if (position == 0) {
+                        v.setPadding(0, 0, 0, insets1.bottom);
+                    } else {
+                        holder.recyclerView.setPadding(0, 0, 0, insets1.bottom);
+                    }
+                    return WindowInsetsCompat.CONSUMED;
+                });
+                if (holder.itemView.isAttachedToWindow()) {
+                    holder.itemView.requestApplyInsets();
+                } else {
+                    holder.itemView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+                        @Override
+                        public void onViewAttachedToWindow(View v) {
+                            v.removeOnAttachStateChangeListener(this);
+                            v.requestApplyInsets();
+                        }
+
+                        @Override
+                        public void onViewDetachedFromWindow(View v) {
+
+                        }
+                    });
+                }
+            }
+            if (position == 0) {
+                binding.appBar.setLiftOnScrollTargetViewId(R.id.scrollView);
+                markwon.setMarkdown(holder.textView, module.getReadme());
+            } else {
+                binding.appBar.setLiftOnScrollTargetViewId(R.id.recyclerView);
+                ReleaseAdapter adapter = new ReleaseAdapter(module.getReleases());
+                holder.recyclerView.setAdapter(adapter);
+                holder.recyclerView.setLayoutManager(new LinearLayoutManagerFix(RepoItemActivity.this));
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return 2;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return position;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView textView;
+            RecyclerView recyclerView;
+
+            public ViewHolder(@NonNull View itemView, int viewType) {
+                super(itemView);
+                if (viewType == 0) {
+                    textView = itemView.findViewById(R.id.readme);
+                } else {
+                    recyclerView = itemView.findViewById(R.id.recyclerView);
+                }
+            }
+        }
+    }
+}
