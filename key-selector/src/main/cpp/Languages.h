e5c3ae47dbd23c52a5efde2c8e56ef209a73de4a
kotori0 <admin@0x.mk>
2021-02-10 23:03:02 +0800

    [key selector] Load default config and adjust timeout

--- key-selector/src/main/cpp/Languages.h
+++ key-selector/src/main/cpp/Languages.h
@@ -11,11 +11,19 @@ public:
     virtual const std::string desc_line_1() {
         return "Select variant. Use Volume Down to move and Volume Up to confirm.";
     }
-    virtual const std::string desc_line_2(uint16_t seconds) {
+    virtual const std::string desc_line_2(const uint16_t seconds) {
         const char base[] = "The program will select YAHFA for you in %hu seconds if you don't \nhave a physical volume button. ";
+        return u16fmt(base, seconds);
+    }
+    virtual const std::string timeout(const uint16_t seconds) {
+        const char base[] = "No operation after %hu seconds.";
+        return u16fmt(base, seconds);
+    };
+protected:
+    std::string u16fmt(const char* base, std::uint16_t s){
         std::string out;
-        out.resize(sizeof(base) + 20);
-        sprintf(out.data(), base, seconds);
+        out.resize(strlen(base) + 20);
+        sprintf(out.data(), base, s);
         return out;
     }
 };
@@ -25,12 +33,13 @@ public:
     const std::string desc_line_1() override {
         return "请选择变种。使用音量减切换，音量加确定。";
     }
-    const std::string desc_line_2(uint16_t seconds) override {
+    const std::string desc_line_2(const uint16_t seconds) override {
         const char base[] = "如果您的设备没有音量键，本程序将会在 %hu 秒后自动选择 YAHFA 。";
-        std::string out;
-        out.resize(sizeof(base) + 20);
-        sprintf(out.data(), base, seconds);
-        return out;
+        return u16fmt(base, seconds);
+    }
+    const std::string timeout(const uint16_t seconds) override {
+        const char base[] = "在 %hu 秒内没有任何操作。";
+        return u16fmt(base, seconds);
     }
 };
 
