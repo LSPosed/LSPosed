0a5052f4e4cbed5fc3f9f85cb6b63d00de4aedaa
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-02-10 16:54:28 +0800

    [app] Add svg support

---
+++ app/src/main/java/io/github/lsposed/manager/util/svg/SvgDecoder.java
@@ -0,0 +1,44 @@
+package io.github.lsposed.manager.util.svg;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.load.Options;
+import com.bumptech.glide.load.ResourceDecoder;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.load.resource.SimpleResource;
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGParseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static com.bumptech.glide.request.target.Target.SIZE_ORIGINAL;
+
+/**
+ * Decodes an SVG internal representation from an {@link InputStream}.
+ */
+public class SvgDecoder implements ResourceDecoder<InputStream, SVG> {
+
+    @Override
+    public boolean handles(@NonNull InputStream source, @NonNull Options options) {
+        // TODO: Can we tell?
+        return true;
+    }
+
+    public Resource<SVG> decode(
+            @NonNull InputStream source, int width, int height, @NonNull Options options)
+            throws IOException {
+        try {
+            SVG svg = SVG.getFromInputStream(source);
+            if (width != SIZE_ORIGINAL) {
+                svg.setDocumentWidth(width);
+            }
+            if (height != SIZE_ORIGINAL) {
+                svg.setDocumentHeight(height);
+            }
+            return new SimpleResource<>(svg);
+        } catch (SVGParseException ex) {
+            throw new IOException("Cannot load SVG from stream", ex);
+        }
+    }
+}
