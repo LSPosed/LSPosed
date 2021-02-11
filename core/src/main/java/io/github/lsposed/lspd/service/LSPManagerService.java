43cd127ded25c9c2154ffc12ed43272a5b29bea2
LoveSy <shana@zju.edu.cn>
2021-02-11 15:04:17 +0800

    Seperate manager service

--- core/src/main/java/io/github/lsposed/lspd/service/LSPManagerService.java
+++ core/src/main/java/io/github/lsposed/lspd/service/LSPManagerService.java
@@ -1,16 +1,19 @@
 package io.github.lsposed.lspd.service;
 
+import android.content.pm.PackageInfo;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 
 import de.robv.android.xposed.XposedBridge;
-import io.github.xposed.xposedservice.IXposedService;
+import io.github.lsposed.lspd.ILSPManagerService;
+import io.github.lsposed.lspd.utils.ParceledListSlice;
 
 import static io.github.lsposed.lspd.service.Service.TAG;
 
-public class LSPosedService extends IXposedService.Stub {
+public class LSPManagerService extends ILSPManagerService.Stub {
 
-    public LSPosedService() {
+    public LSPManagerService() {
         BridgeService.send(this, new BridgeService.Listener() {
             @Override
             public void onSystemServerRestarted() {
@@ -27,7 +30,6 @@ public class LSPosedService extends IXposedService.Stub {
             }
         });
     }
-
     @Override
     public IBinder asBinder() {
         return this;
@@ -36,5 +38,10 @@ public class LSPosedService extends IXposedService.Stub {
     @Override
     public int getVersion() {
         return XposedBridge.getXposedVersion();
+    }
+
+    @Override
+    public ParceledListSlice<PackageInfo> getInstalledPackagesFromAllUsers(int flags) throws RemoteException {
+        return PackageService.getInstalledPackagesFromAllUsers(flags);
     }
 }
