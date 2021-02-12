b9247ad4244f34df441b968d0a238db35e195658
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-02-12 22:41:24 +0800

    [app] Fix status dialog

--- app/src/main/java/io/github/lsposed/manager/ui/fragment/StatusDialogBuilder.java
+++ app/src/main/java/io/github/lsposed/manager/ui/fragment/StatusDialogBuilder.java
@@ -1,13 +1,13 @@
 package io.github.lsposed.manager.ui.fragment;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.text.HtmlCompat;
 
 import java.io.BufferedReader;
