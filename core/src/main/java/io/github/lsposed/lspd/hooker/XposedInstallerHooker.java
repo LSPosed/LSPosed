a652a81201a093407951da01dd4db0ba844fa52a
LoveSy <shana@zju.edu.cn>
2021-02-14 02:16:19 +0800

    Use setStaticField instead

--- core/src/main/java/io/github/lsposed/lspd/hooker/XposedInstallerHooker.java
+++ core/src/main/java/io/github/lsposed/lspd/hooker/XposedInstallerHooker.java
@@ -32,26 +32,41 @@ import io.github.lsposed.lspd.util.Utils;
 public class XposedInstallerHooker {
 
     public static void hookXposedInstaller(final ClassLoader classLoader) {
-        String variant_;
+        final String variant;
         switch (Main.getEdxpVariant()) {
             case EdxpImpl.YAHFA:
-                variant_ = "YAHFA";
+                variant = "YAHFA";
                 break;
             case EdxpImpl.SANDHOOK:
-                variant_ = "SandHook";
+                variant = "SandHook";
                 break;
             case EdxpImpl.NONE:
             default:
-                variant_ = "Unknown";
+                variant = "Unknown";
                 break;
         }
-        final String variant = variant_;
 
         Utils.logI("Found LSPosed Manager, hooking it");
 
         // LSPosed Manager R
         try {
             Class<?> ConstantsClass = XposedHelpers.findClass("io.github.lsposed.manager.Constants", classLoader);
+            try {
+                XposedHelpers.setStaticIntField(ConstantsClass, "xposedApiVersion", XposedBridge.getXposedVersion());
+                XposedHelpers.setStaticObjectField(ConstantsClass, "xposedVersion", BuildConfig.VERSION_NAME);
+                XposedHelpers.setStaticIntField(ConstantsClass, "xposedVersionCode", BuildConfig.VERSION_CODE);
+                XposedHelpers.setStaticIntField(ConstantsClass, "xposedVersionCode", XposedBridge.getXposedVersion());
+                XposedHelpers.setStaticObjectField(ConstantsClass, "xposedVariant", variant);
+                XposedHelpers.setStaticObjectField(ConstantsClass, "baseDir", ConfigManager.getBaseConfigPath() + "/");
+                XposedHelpers.setStaticObjectField(ConstantsClass, "logDir", ConfigManager.getLogPath());
+                XposedHelpers.setStaticObjectField(ConstantsClass, "miscDir", ConfigManager.getMiscPath());
+                XposedHelpers.setStaticBooleanField(ConstantsClass, "permissive", ConfigManager.isPermissive());
+
+                Utils.logI("Hooked LSPosed Manager");
+                return;
+            } catch (Throwable ignored) {
+                // fallback
+            }
             XposedHelpers.findAndHookMethod(ConstantsClass, "getXposedApiVersion", new XC_MethodReplacement() {
                 @Override
                 protected Object replaceHookedMethod(MethodHookParam param) {
@@ -68,12 +83,6 @@ public class XposedInstallerHooker {
                 @Override
                 protected Object replaceHookedMethod(MethodHookParam param) {
                     return BuildConfig.VERSION_CODE;
-                }
-            });
-            XposedHelpers.findAndHookMethod(ConstantsClass, "getXposedApiVersion", new XC_MethodReplacement() {
-                @Override
-                protected Object replaceHookedMethod(MethodHookParam param) {
-                    return XposedBridge.getXposedVersion();
                 }
             });
             XposedHelpers.findAndHookMethod(ConstantsClass, "getXposedVariant", new XC_MethodReplacement() {
