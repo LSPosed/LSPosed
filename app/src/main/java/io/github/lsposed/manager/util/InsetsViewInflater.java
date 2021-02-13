4c740ea551f4402ca12281a3c59f2702efac800f
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-02-13 12:51:31 +0800

    [app] Update copyright header

--- app/src/main/java/io/github/lsposed/manager/util/InsetsViewInflater.java
+++ app/src/main/java/io/github/lsposed/manager/util/InsetsViewInflater.java
@@ -1,21 +1,21 @@
 /*
- * This file is part of EdXposed.
+ * This file is part of LSPosed.
  *
- * EdXposed is free software: you can redistribute it and/or modify
+ * LSPosed is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * EdXposed is distributed in the hope that it will be useful,
+ * LSPosed is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with EdXposed.  If not, see <https://www.gnu.org/licenses/>.
+ * along with LSPosed.  If not, see <https://www.gnu.org/licenses/>.
  *
  * Copyright (C) 2020 EdXposed Contributors
- * Copyright (C) 2021 EdXposed Contributors
+ * Copyright (C) 2021 LSPosed Contributors
  */
 
 package io.github.lsposed.manager.util;
