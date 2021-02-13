e507c8262fcb263d602754b41a862d29910b1432
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-02-13 12:51:31 +0800

    [app] Update copyright header

--- app/src/main/java/io/github/lsposed/manager/util/CustomThemeColor.java
+++ app/src/main/java/io/github/lsposed/manager/util/CustomThemeColor.java
@@ -1,6 +1,21 @@
 /*
- * Copyright (c) 2019 Hai Zhang <dreaming.in.code.zh@gmail.com>
- * All Rights Reserved.
+ * This file is part of LSPosed.
+ *
+ * LSPosed is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LSPosed is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LSPosed.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2020 EdXposed Contributors
+ * Copyright (C) 2021 LSPosed Contributors
  */
 
 package io.github.lsposed.manager.util;
