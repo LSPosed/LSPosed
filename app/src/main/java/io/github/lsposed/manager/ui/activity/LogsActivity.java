ad7f1d1b989593481db73619a4d38fa02fe255a4
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-02-25 16:49:11 +0800

    [app] Fix log file name for saving and sending (#199)

--- app/src/main/java/io/github/lsposed/manager/ui/activity/LogsActivity.java
+++ app/src/main/java/io/github/lsposed/manager/ui/activity/LogsActivity.java
@@ -237,7 +237,8 @@ public class LogsActivity extends BaseActivity {
         }
         Calendar now = Calendar.getInstance();
         String filename = String.format(Locale.US,
-                "LSPosed_Verbose_%04d%02d%02d_%02d%02d%02d.log",
+                "LSPosed_%s_%04d%02d%02d_%02d%02d%02d.log",
+                verbose ? "Verbose" : "Modules",
                 now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1,
                 now.get(Calendar.DAY_OF_MONTH), now.get(Calendar.HOUR_OF_DAY),
                 now.get(Calendar.MINUTE), now.get(Calendar.SECOND));
@@ -263,7 +264,8 @@ public class LogsActivity extends BaseActivity {
     private void save() {
         Calendar now = Calendar.getInstance();
         String filename = String.format(Locale.US,
-                "LSPosed_Verbose_%04d%02d%02d_%02d%02d%02d.log",
+                "LSPosed_%s_%04d%02d%02d_%02d%02d%02d.log",
+                verbose ? "Verbose" : "Modules",
                 now.get(Calendar.YEAR), now.get(Calendar.MONTH) + 1,
                 now.get(Calendar.DAY_OF_MONTH), now.get(Calendar.HOUR_OF_DAY),
                 now.get(Calendar.MINUTE), now.get(Calendar.SECOND));
