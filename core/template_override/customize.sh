620b08c050b32a65347559fa0b786414b9f0146e
LoveSy <shana@zju.edu.cn>
2021-03-07 02:46:57 +0800

    [core] Merge manager into Magisk zip

--- core/template_override/customize.sh
+++ core/template_override/customize.sh
@@ -197,6 +197,7 @@ touch /data/adb/lspd/new_install || abortC "! ${LANG_CUST_ERR_CONF_FIRST}"
 ui_print "- ${LANG_CUST_INST_COPY_LIB}"
 rm -rf "/data/adb/lspd/framework"
 mv "${MODPATH}/system/framework" "/data/adb/lspd/framework"
+extract "${ZIPFILE}" 'manager.apk' "/data/adb/lspd/"
 mkdir -p /data/misc/$MISC_PATH
 set_perm /data/misc/$MISC_PATH 0 0 0771 "u:object_r:magisk_file:s0" || abortC "! ${LANG_CUST_ERR_PERM}"
 
