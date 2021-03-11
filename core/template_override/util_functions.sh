b50ae2ef3b696c728090392b0e1dea50a0355b42
LoveSy <shana@zju.edu.cn>
2021-03-11 23:41:22 +0800

    Support riru 24.0.1

--- core/template_override/util_functions.sh
+++ core/template_override/util_functions.sh
@@ -23,17 +23,20 @@ RIRU_MODULE_API_VERSION=%%%RIRU_MODULE_API_VERSION%%%
 RIRU_MODULE_MIN_API_VERSION=%%%RIRU_MODULE_MIN_API_VERSION%%%
 RIRU_MODULE_MIN_RIRU_VERSION_NAME="%%%RIRU_MODULE_MIN_RIRU_VERSION_NAME%%%"
 
+if [ "$MAGISK_VER_CODE" -ge 21000 ]; then
+  MAGISK_CURRENT_RIRU_MODULE_PATH=$(magisk --path)/.magisk/modules/riru-core
+else
+  MAGISK_CURRENT_RIRU_MODULE_PATH=/sbin/.magisk/modules/riru-core
+fi
 
 check_riru_version() {
-  RIRU_CORE_MODULES_PATH=/data/adb/modules/riru-core
-  RIRU_CORE_MODULES_UPDATE_PATH=/data/adb/modules_update/riru-core
-  if [ ! -f "$RIRU_CORE_MODULES_UPDATE_PATH/api_version" ] && [ ! -f "$RIRU_CORE_MODULES_PATH/api_version" ]; then
+  if [ ! -f "$MAGISK_CURRENT_RIRU_MODULE_PATH/api_version" ] && [ ! -f "/data/adb/riru/api_version" ] && [ ! -f "/data/adb/riru/api_version.new" ]; then
     ui_print "*********************************************************"
     ui_print "! Riru $RIRU_MODULE_MIN_RIRU_VERSION_NAME or above is required"
     ui_print "! Please install Riru from Magisk Manager or https://github.com/RikkaApps/Riru/releases"
     abort "*********************************************************"
   fi
-  RIRU_API=$(cat "$RIRU_CORE_MODULES_UPDATE_PATH/api_version") || RIRU_API=$(cat "$RIRU_CORE_MODULES_PATH/api_version") || RIRU_API=0
+  RIRU_API=$(cat "$MAGISK_CURRENT_RIRU_MODULE_PATH/api_version") || RIRU_API=$(cat "/data/adb/riru/api_version.new") || RIRU_API=$(cat "/data/adb/riru/api_version") || RIRU_API=0
   [ "$RIRU_API" -eq "$RIRU_API" ] || RIRU_API=0
   ui_print "- Riru API version: $RIRU_API"
   if [ "$RIRU_API" -lt $RIRU_MODULE_MIN_API_VERSION ]; then
