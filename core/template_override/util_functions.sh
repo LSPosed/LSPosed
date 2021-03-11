edf41b715f5fe65c89bace9f33d8c98a5cafe80f
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-03-11 19:23:01 +0800

    [core] Fix riru version check

--- core/template_override/util_functions.sh
+++ core/template_override/util_functions.sh
@@ -27,13 +27,13 @@ RIRU_MODULE_MIN_RIRU_VERSION_NAME="%%%RIRU_MODULE_MIN_RIRU_VERSION_NAME%%%"
 check_riru_version() {
   RIRU_CORE_MODULES_PATH=/data/adb/modules/riru-core
   RIRU_CORE_MODULES_UPDATE_PATH=/data/adb/modules_update/riru-core
-  if [ ! -f "$RIRU_CORE_MODULES_UPDATE_PATH/api_version" ] && [ ! -f "$RIRU_CORE_MODULES_PATH/api_version" ] && [ ! -f "/data/adb/riru/api_version" ] && [ ! -f "/data/adb/riru/api_version.new" ]; then
+  if [ ! -f "$RIRU_CORE_MODULES_UPDATE_PATH/api_version" ] && [ ! -f "$RIRU_CORE_MODULES_PATH/api_version" ]; then
     ui_print "*********************************************************"
     ui_print "! Riru $RIRU_MODULE_MIN_RIRU_VERSION_NAME or above is required"
     ui_print "! Please install Riru from Magisk Manager or https://github.com/RikkaApps/Riru/releases"
     abort "*********************************************************"
   fi
-  RIRU_API=$(cat "$RIRU_CORE_MODULES_UPDATE_PATH/api_version") || RIRU_API=$(cat "$RIRU_CORE_MODULES_PATH/api_version") || RIRU_API=$(cat "/data/adb/riru/api_version.new") || RIRU_API=$(cat "/data/adb/riru/api_version") || RIRU_API=0
+  RIRU_API=$(cat "$RIRU_CORE_MODULES_UPDATE_PATH/api_version") || RIRU_API=$(cat "$RIRU_CORE_MODULES_PATH/api_version") || RIRU_API=0
   [ "$RIRU_API" -eq "$RIRU_API" ] || RIRU_API=0
   ui_print "- Riru API version: $RIRU_API"
   if [ "$RIRU_API" -lt $RIRU_MODULE_MIN_API_VERSION ]; then
--- core/tpl/module.prop.tpl
+++ core/tpl/module.prop.tpl
@@ -3,6 +3,4 @@ name=Riru - LSPosed
 version=${versionName}
 versionCode=${versionCode}
 author=${authorList}
-api=${apiCode}
 description=Another enhanced implementation of Xposed Framework. Supports Android 8.1 ~ 12 DP1. Requires Riru v23 or above installed.
-minApi=${minApi}
