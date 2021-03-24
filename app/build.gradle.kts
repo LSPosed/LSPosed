65cec874d6b85b6772c9db91ef4beb2ec56691ab
Wang Han <wanghan1995315@gmail.com>
2021-03-24 09:49:02 +0800

    [app] Update RikkaX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -189,7 +189,7 @@ dependencies {
     implementation("dev.rikka.rikkax.appcompat:appcompat:1.2.0-rc01")
     implementation("dev.rikka.rikkax.core:core:1.3.2")
     implementation("dev.rikka.rikkax.insets:insets:1.0.1")
-    implementation("dev.rikka.rikkax.material:material:1.6.1")
+    implementation("dev.rikka.rikkax.material:material:1.6.4")
     implementation("dev.rikka.rikkax.preference:simplemenu-preference:1.0.2")
     implementation("dev.rikka.rikkax.recyclerview:recyclerview-ktx:1.2.0")
     implementation("dev.rikka.rikkax.widget:borderview:1.0.1")
