0f4c9f317375efc123aeee2fc7797ba2640b75a4
LoveSy <shana@zju.edu.cn>
2021-03-26 23:24:40 +0800

    Github.sha as cache key

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -167,6 +167,7 @@ task("buildLibcxx", Exec::class) {
         environment("NDK_CCACHE", it)
         environment("USE_CCACHE", "1")
         environment("CCACHE_COMPILERCHECK", "content")
+        environment("CCACHE_MAXSIZE", "100M")
     } ?: run {
         println("not using ccache")
     }
