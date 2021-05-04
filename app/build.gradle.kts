5d732da0ade6dd68374c091f57cd8ed494e7edd0
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-05-04 11:29:29 +0800

    [app] Update RikkaX material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -203,7 +203,7 @@ dependencies {
     implementation("dev.rikka.rikkax.appcompat:appcompat:1.2.0-rc01")
     implementation("dev.rikka.rikkax.core:core:1.3.2")
     implementation("dev.rikka.rikkax.insets:insets:1.0.1")
-    implementation("dev.rikka.rikkax.material:material:1.6.4")
+    implementation("dev.rikka.rikkax.material:material:1.6.5")
     implementation("dev.rikka.rikkax.preference:simplemenu-preference:1.0.2")
     implementation("dev.rikka.rikkax.recyclerview:recyclerview-ktx:1.2.1")
     implementation("dev.rikka.rikkax.widget:borderview:1.0.1")
