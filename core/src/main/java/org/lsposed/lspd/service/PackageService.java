805f71781639e292ff9c67e76046deafa7c27c28
Wang Han <wanghan1995315@gmail.com>
2021-05-15 20:31:38 +0800

    Drop INSTALL_ALL_WHITELIST_RESTRICTED_PERMISSIONS

--- core/src/main/java/org/lsposed/lspd/service/PackageService.java
+++ core/src/main/java/org/lsposed/lspd/service/PackageService.java
@@ -68,7 +68,6 @@ public class PackageService {
 
     static final int INSTALL_FAILED_INTERNAL_ERROR = -110;
     static final int INSTALL_REASON_UNKNOWN = 0;
-    static final int INSTALL_ALL_WHITELIST_RESTRICTED_PERMISSIONS = 0x00400000;
     static final int INSTALL_PARSE_FAILED_BAD_PACKAGE_NAME = -106;
 
 
@@ -259,9 +258,9 @@ public class PackageService {
         IPackageManager pm = getPackageManager();
         if (pm == null) return INSTALL_FAILED_INTERNAL_ERROR;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            return pm.installExistingPackageAsUser(packageName, userId, INSTALL_REASON_UNKNOWN, INSTALL_ALL_WHITELIST_RESTRICTED_PERMISSIONS, null);
+            return pm.installExistingPackageAsUser(packageName, userId, INSTALL_REASON_UNKNOWN, 0, null);
         } else {
-            return pm.installExistingPackageAsUser(packageName, userId, INSTALL_REASON_UNKNOWN, INSTALL_ALL_WHITELIST_RESTRICTED_PERMISSIONS);
+            return pm.installExistingPackageAsUser(packageName, userId, INSTALL_REASON_UNKNOWN, 0);
         }
     }
 
