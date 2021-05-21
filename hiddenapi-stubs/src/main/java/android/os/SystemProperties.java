41ab411cdc4da1115d90099625909de45e4b06cb
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-05-21 11:30:54 +0800

    [core] Check if MIUI

---
+++ hiddenapi-stubs/src/main/java/android/os/SystemProperties.java
@@ -0,0 +1,11 @@
+package android.os;
+
+public class SystemProperties {
+    public static String get(String key) {
+        throw new UnsupportedOperationException("Stub");
+    }
+
+    public static void set(String key, String val) {
+        throw new UnsupportedOperationException("Stub");
+    }
+}
