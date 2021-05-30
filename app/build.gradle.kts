5da046edab588f7e32771b60a7567c9b55a00f13
LoveSy <shana@zju.edu.cn>
2021-05-30 21:47:56 +0800

    [app] Update RikkaX SimpleMenuPreference (#702)
    Co-authored-by: tehcneko <7764726+tehcneko@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -222,7 +222,7 @@ dependencies {
     implementation("dev.rikka.rikkax.core:core:1.3.2")
     implementation("dev.rikka.rikkax.insets:insets:1.1.0")
     implementation("dev.rikka.rikkax.material:material:1.6.5")
-    implementation("dev.rikka.rikkax.preference:simplemenu-preference:1.0.2")
+    implementation("dev.rikka.rikkax.preference:simplemenu-preference:1.0.3")
     implementation("dev.rikka.rikkax.recyclerview:recyclerview-ktx:1.2.1")
     implementation("dev.rikka.rikkax.widget:borderview:1.0.1")
     implementation("dev.rikka.rikkax.widget:switchbar:1.0.2")
