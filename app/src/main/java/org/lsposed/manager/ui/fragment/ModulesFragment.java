2339fb73782c5c27606c8d61256ddc5fef225a6c
tehcneko <7764726+tehcneko@users.noreply.github.com>
2021-06-02 22:27:14 +0800

    Update ModulesFragment.java

--- app/src/main/java/org/lsposed/manager/ui/fragment/ModulesFragment.java
+++ app/src/main/java/org/lsposed/manager/ui/fragment/ModulesFragment.java
@@ -138,7 +138,7 @@ public class ModulesFragment extends BaseFragment implements ModuleUtil.ModuleLi
 
         binding.getRoot().bringChildToFront(binding.appBar);
         setupToolbar(binding.toolbar, R.string.Modules, R.menu.menu_modules);
-        binding.viewPager.setAdapter(new PagerAdapter());
+        binding.viewPager.setAdapter(pagerAdapter);
         binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
