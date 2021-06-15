eeeebe1ca113c5e17f523b420a548578f375c33c
Wang Han <wanghan1995315@gmail.com>
2021-06-15 17:56:12 +0800

    [app] Update dependencies (#748)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -224,7 +224,7 @@ dependencies {
     implementation("dev.rikka.rikkax.insets:insets:1.1.0")
     implementation("dev.rikka.rikkax.material:material:1.6.5")
     implementation("dev.rikka.rikkax.preference:simplemenu-preference:1.0.3")
-    implementation("dev.rikka.rikkax.recyclerview:recyclerview-ktx:1.2.1")
+    implementation("dev.rikka.rikkax.recyclerview:recyclerview-ktx:1.2.2")
     implementation("dev.rikka.rikkax.widget:borderview:1.0.1")
     implementation("dev.rikka.rikkax.widget:switchbar:1.0.2")
     implementation("dev.rikka.rikkax.layoutinflater:layoutinflater:1.0.1")
@@ -236,7 +236,7 @@ dependencies {
     implementation("io.noties.markwon:image-glide:$markwonVersion")
     implementation("io.noties.markwon:linkify:$markwonVersion")
     implementation("me.zhanghai.android.appiconloader:appiconloader-glide:1.3.1")
-    implementation("me.zhanghai.android.fastscroll:library:1.1.5")
+    implementation("me.zhanghai.android.fastscroll:library:1.1.6")
     implementation("org.lsposed.hiddenapibypass:hiddenapibypass:2.0")
     implementation(project(":manager-service"))
 }
