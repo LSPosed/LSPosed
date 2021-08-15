bb558d81522efed57c16a594c1ad5e78f37b6526
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2021-08-15 22:35:57 +0800

    [app] Simplify sort menu (#905)

--- app/src/main/java/org/lsposed/manager/adapters/ScopeAdapter.java
+++ app/src/main/java/org/lsposed/manager/adapters/ScopeAdapter.java
@@ -324,25 +324,29 @@ public class ScopeAdapter extends RecyclerView.Adapter<ScopeAdapter.ViewHolder> 
         menu.findItem(R.id.item_filter_modules).setChecked(preferences.getBoolean("filter_modules", true));
         switch (preferences.getInt("list_sort", 0)) {
             case 7:
-                menu.findItem(R.id.item_sort_by_update_time_reverse).setChecked(true);
+                menu.findItem(R.id.item_sort_by_update_time).setChecked(true);
+                menu.findItem(R.id.reverse).setChecked(true);
                 break;
             case 6:
                 menu.findItem(R.id.item_sort_by_update_time).setChecked(true);
                 break;
             case 5:
-                menu.findItem(R.id.item_sort_by_install_time_reverse).setChecked(true);
+                menu.findItem(R.id.item_sort_by_install_time).setChecked(true);
+                menu.findItem(R.id.reverse).setChecked(true);
                 break;
             case 4:
                 menu.findItem(R.id.item_sort_by_install_time).setChecked(true);
                 break;
             case 3:
-                menu.findItem(R.id.item_sort_by_package_name_reverse).setChecked(true);
+                menu.findItem(R.id.item_sort_by_package_name).setChecked(true);
+                menu.findItem(R.id.reverse).setChecked(true);
                 break;
             case 2:
                 menu.findItem(R.id.item_sort_by_package_name).setChecked(true);
                 break;
             case 1:
-                menu.findItem(R.id.item_sort_by_name_reverse).setChecked(true);
+                menu.findItem(R.id.item_sort_by_name).setChecked(true);
+                menu.findItem(R.id.reverse).setChecked(true);
                 break;
             case 0:
                 menu.findItem(R.id.item_sort_by_name).setChecked(true);
