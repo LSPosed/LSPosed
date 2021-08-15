ab050cefb9c06a21416cfb45d6a0ffe4fcfd2486
vvb2060 <vvb2060@gmail.com>
2021-08-15 12:46:23 +0800

    [core] Remove workaround
    close #856

--- hiddenapi-bridge/src/main/java/hidden/ByteBufferDexClassLoader.java
+++ hiddenapi-bridge/src/main/java/hidden/ByteBufferDexClassLoader.java
@@ -1,31 +1,10 @@
 package hidden;
 
-import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 
 import dalvik.system.BaseDexClassLoader;
 
 public class ByteBufferDexClassLoader extends BaseDexClassLoader {
-    static Field pathListField = null;
-    static Field dexElementsField = null;
-    static Field dexFileField = null;
-    static Field nameField = null;
-
-    static {
-        try {
-            pathListField = BaseDexClassLoader.class.getDeclaredField("pathList");
-            pathListField.setAccessible(true);
-            dexElementsField = pathListField.getType().getDeclaredField("dexElements");
-            dexElementsField.setAccessible(true);
-            var elementType = dexElementsField.getType().getComponentType();
-            dexFileField = elementType.getDeclaredField("dexFile");
-            dexFileField.setAccessible(true);
-            nameField = dexFileField.getType().getDeclaredField("mFileName");
-            nameField.setAccessible(true);
-        } catch (Throwable ignored) {
-
-        }
-    }
 
     public ByteBufferDexClassLoader(ByteBuffer[] dexFiles, ClassLoader parent) {
         super(dexFiles, parent);
@@ -33,17 +12,6 @@ public class ByteBufferDexClassLoader extends BaseDexClassLoader {
 
     public ByteBufferDexClassLoader(ByteBuffer[] dexFiles, String librarySearchPath, ClassLoader parent) {
         super(dexFiles, librarySearchPath, parent);
-    }
-
-    // Some modules get their module paths from this variable
-    // They should use `initZygote.modulePath` instead
-    // Temporarily workaround
-    // TODO(vvb2060): removed in the next major release
-    public void setDexName(String name){
-        try {
-            nameField.set(dexFileField.get(((Object[]) dexElementsField.get(pathListField.get(this)))[0]), name);
-        } catch (Throwable ignored) {
-        }
     }
 
     public String getLdLibraryPath() {
--- hiddenapi-stubs/src/main/java/dalvik/system/BaseDexClassLoader.java
+++ hiddenapi-stubs/src/main/java/dalvik/system/BaseDexClassLoader.java
@@ -1,19 +1,8 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 package dalvik.system;
 
-import java.io.File;
 import java.nio.ByteBuffer;
 
 public class BaseDexClassLoader extends ClassLoader {
-    private Object pathList;
-    public BaseDexClassLoader(String dexPath, File optimizedDirectory, String librarySearchPath, ClassLoader parent) {
-        throw new RuntimeException("Stub!");
-    }
-
     public BaseDexClassLoader(ByteBuffer[] dexFiles, ClassLoader parent) {
         throw new RuntimeException("Stub!");
     }
