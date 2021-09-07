6c170ab4eb8f0297ae63afebb2a74ec7bec943af
tehcneko <88844448+tehcneko@users.noreply.github.com>
2021-09-07 17:56:43 +0800

    [app] Update home design (#1076)

--- app/src/main/java/org/lsposed/manager/ui/fragment/ModulesFragment.java
+++ app/src/main/java/org/lsposed/manager/ui/fragment/ModulesFragment.java
@@ -23,7 +23,6 @@ import static android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS;
 import static androidx.recyclerview.widget.RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
