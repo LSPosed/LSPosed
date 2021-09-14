e250b73f99300e3db6447e848164954cdf9583e2
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2021-09-14 12:58:32 +0800

    [app] Fix hide translation contributors preference (#1108)

--- app/src/main/java/org/lsposed/manager/ui/fragment/SettingsFragment.java
+++ app/src/main/java/org/lsposed/manager/ui/fragment/SettingsFragment.java
@@ -283,7 +283,7 @@ public class SettingsFragment extends BaseFragment {
             Preference translation_contributors = findPreference("translation_contributors");
             if (translation_contributors != null) {
                 var translators = HtmlCompat.fromHtml(getString(R.string.translators), HtmlCompat.FROM_HTML_MODE_LEGACY);
-                if (translators.equals("null")) {
+                if (translators.toString().equals("null")) {
                     translation_contributors.setVisible(false);
                 } else {
                     translation_contributors.setSummary(translators);
