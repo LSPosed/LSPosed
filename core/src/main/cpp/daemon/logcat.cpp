f06d9726074368f2109640e3631f83f96cd86041
vvb2060 <vvb2060@gmail.com>
2021-09-24 19:55:31 +0800

    [core] check log size before set

--- core/src/main/cpp/daemon/logcat.cpp
+++ core/src/main/cpp/daemon/logcat.cpp
@@ -11,12 +11,7 @@
 using namespace std::string_view_literals;
 
 constexpr size_t kMaxLogSize = 4 * 1024 * 1024;
-
-#ifndef NDEBUG
-    constexpr size_t kLogBufferSize = 1 * 1024 * 1024;
-#else
-    constexpr size_t kLogBufferSize = 256 * 1024;
-#endif
+constexpr size_t kLogBufferSize = 256 * 1024;
 
 constexpr std::array<char, ANDROID_LOG_SILENT + 1> kLogChar = {
         /*ANDROID_LOG_UNKNOWN*/'?',
@@ -164,7 +159,9 @@ void Logcat::Run() {
         for (log_id id:{LOG_ID_MAIN, LOG_ID_CRASH}) {
             auto *logger = android_logger_open(logger_list.get(), id);
             if (logger == nullptr) continue;
-            android_logger_set_log_size(logger, kLogBufferSize);
+            if (android_logger_get_log_size(logger) < kLogBufferSize) {
+                android_logger_set_log_size(logger, kLogBufferSize);
+            }
         }
 
         struct log_msg msg{};
