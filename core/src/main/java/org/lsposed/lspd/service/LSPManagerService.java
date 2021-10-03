5a3b7c1c5914d4da3f72afc04f8a66da09ffec0b
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2021-10-03 10:49:27 +0800

    [core] Fix the module scope setting on first installation

--- core/src/main/java/org/lsposed/lspd/service/LSPManagerService.java
+++ core/src/main/java/org/lsposed/lspd/service/LSPManagerService.java
@@ -518,7 +518,7 @@ public class LSPManagerService extends ILSPManagerService.Stub {
     }
 
     @Override
-    public boolean setModuleScope(String packageName, ParceledListSlice<Application> scope) {
+    public boolean setModuleScope(String packageName, ParceledListSlice<Application> scope) throws RemoteException {
         return ConfigManager.getInstance().setModuleScope(packageName, scope.getList());
     }
 
