fe376de955acefd9b7b880d4e4ef9f26e66e6ac8
LoveSy <shana@zju.edu.cn>
2021-10-11 18:10:56 +0800

    [core] Fix settings on Android 12

--- hiddenapi-stubs/src/main/java/android/content/IContentProvider.java
+++ hiddenapi-stubs/src/main/java/android/content/IContentProvider.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.os.IInterface;
 import android.os.RemoteException;
 
+import androidx.annotation.RequiresApi;
+
 public interface IContentProvider extends IInterface {
     Bundle call(String callingPkg, String attributionTag, String authority,
                 String method, String arg, Bundle extras) throws RemoteException;
 
+    @RequiresApi(31)
+    Bundle call(AttributionSource attributionSource, String authority,
+                String method, String arg, Bundle extras) throws RemoteException;
 }
