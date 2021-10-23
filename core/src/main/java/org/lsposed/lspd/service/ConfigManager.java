1d370d8006e8a005e2d5b818fbd35b65977b85c9
Wang Han <wanghan1995315@gmail.com>
2021-10-23 15:45:04 +0800

    Update ConfigManager.java

--- core/src/main/java/org/lsposed/lspd/service/ConfigManager.java
+++ core/src/main/java/org/lsposed/lspd/service/ConfigManager.java
@@ -929,6 +929,7 @@ public class ConfigManager {
 
     public List<String> getDenyListPackages() {
         List<String> result = new ArrayList<>();
+        if (!getApi().equals("Zygisk")) return result;
         try (final SQLiteDatabase magiskDb =
                      SQLiteDatabase.openDatabase(ConfigFileManager.magiskDbPath, new SQLiteDatabase.OpenParams.Builder().addOpenFlags(SQLiteDatabase.OPEN_READONLY).build())) {
             try (Cursor cursor = magiskDb.query(true, "denylist", new String[]{"package_name"}, null, null, null, null, null, null, null)) {
