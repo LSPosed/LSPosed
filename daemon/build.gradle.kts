16e1de76ffdf882c92b1af9624b4169adec35de3
Nullptr <tsng.nullptr@outlook.com>
2021-12-30 22:48:15 +0800

    [daemon] Minify (#1507)

--- daemon/build.gradle.kts
+++ daemon/build.gradle.kts
@@ -89,7 +89,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles("proguard-rules.pro")
         }
     }
