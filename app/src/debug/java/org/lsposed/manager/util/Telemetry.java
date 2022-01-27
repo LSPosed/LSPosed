270251f2498a91877bfc1775afaf6e436e83f136
LoveSy <shana@zju.edu.cn>
2022-01-28 05:12:43 +0800

    Fix telemetry version for debug build

--- app/src/debug/java/org/lsposed/manager/util/Telemetry.java
+++ app/src/debug/java/org/lsposed/manager/util/Telemetry.java
@@ -1,28 +1,47 @@
 package org.lsposed.manager.util;
 
 import android.app.Application;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.channel.AbstractChannelListener;
+import com.microsoft.appcenter.channel.Channel;
 import com.microsoft.appcenter.crashes.AbstractCrashesListener;
 import com.microsoft.appcenter.crashes.Crashes;
 import com.microsoft.appcenter.crashes.model.ErrorReport;
 
+import org.lsposed.manager.App;
 import org.lsposed.manager.BuildConfig;
-
-import java.util.Locale;
 
 public class Telemetry {
     public static void start(Application app) {
-        Crashes.setListener(new AbstractCrashesListener() {
-            @Override
-            public void onBeforeSending(ErrorReport report) {
-                var d = report.getDevice();
-                d.setAppVersion(BuildConfig.VERSION_NAME);
-                d.setAppBuild(String.valueOf(BuildConfig.VERSION_CODE));
-                d.setAppNamespace(BuildConfig.APPLICATION_ID);
-                report.setDevice(d);
-            }
-        });
         AppCenter.start(app, "eb3c4175-e879-4312-a72e-b0e64bca142c", Crashes.class);
+        try {
+            var handle = AppCenter.class.getDeclaredField("mHandler");
+            handle.setAccessible(true);
+            ((Handler) handle.get(AppCenter.getInstance())).post(() -> {
+                try {
+                    var channel = AppCenter.class.getDeclaredField("mChannel");
+                    channel.setAccessible(true);
+                    ((Channel) channel.get(AppCenter.getInstance())).addListener(new AbstractChannelListener() {
+                        @Override
+                        public void onPreparedLog(@NonNull com.microsoft.appcenter.ingestion.models.Log log, @NonNull String groupName, int flags) {
+                            var d = log.getDevice();
+                            d.setAppVersion(BuildConfig.VERSION_NAME);
+                            d.setAppBuild(String.valueOf(BuildConfig.VERSION_CODE));
+                            d.setAppNamespace(BuildConfig.APPLICATION_ID);
+                            log.setDevice(d);
+                        }
+                    });
+                } catch (Throwable e) {
+                    Log.e(App.TAG, "add listener", e);
+                }
+            });
+        } catch (Throwable e) {
+            Log.w(App.TAG, "get handler", e);
+        }
     }
 }
