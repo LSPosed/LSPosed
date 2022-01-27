bbe533351039bd16523f5b93e09533fec064750f
LoveSy <shana@zju.edu.cn>
2022-01-27 15:51:58 +0800

    1

--- app/src/debug/java/org/lsposed/manager/util/Telemetry.java
+++ app/src/debug/java/org/lsposed/manager/util/Telemetry.java
@@ -3,13 +3,26 @@ package org.lsposed.manager.util;
 import android.app.Application;
 
 import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.AbstractCrashesListener;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.crashes.model.ErrorReport;
 
+import org.lsposed.manager.BuildConfig;
+
+import java.util.Locale;
 
 public class Telemetry {
-    public static void start(Application context) {
-        AppCenter.start(context, "eb3c4175-e879-4312-a72e-b0e64bca142c",
-                Analytics.class, Crashes.class);
+    public static void start(Application app) {
+        Crashes.setListener(new AbstractCrashesListener() {
+            @Override
+            public void onBeforeSending(ErrorReport report) {
+                var d = report.getDevice();
+                d.setAppVersion(String.format(Locale.ROOT, "%s (%d)", BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE));
+                d.setAppBuild(String.valueOf(BuildConfig.VERSION_CODE));
+                d.setAppNamespace(BuildConfig.APPLICATION_ID);
+                report.setDevice(d);
+            }
+        });
+        AppCenter.start(app, "eb3c4175-e879-4312-a72e-b0e64bca142c", Crashes.class);
     }
 }
