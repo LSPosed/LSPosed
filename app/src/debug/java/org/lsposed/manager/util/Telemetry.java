4fae8404e32f8f3cc79c5e1e81aefc14156391ad
LoveSy <shana@zju.edu.cn>
2022-01-28 14:26:11 +0800

    Refine

--- app/src/debug/java/org/lsposed/manager/util/Telemetry.java
+++ app/src/debug/java/org/lsposed/manager/util/Telemetry.java
@@ -9,39 +9,45 @@ import androidx.annotation.NonNull;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.channel.AbstractChannelListener;
 import com.microsoft.appcenter.channel.Channel;
-import com.microsoft.appcenter.crashes.AbstractCrashesListener;
 import com.microsoft.appcenter.crashes.Crashes;
-import com.microsoft.appcenter.crashes.model.ErrorReport;
 
 import org.lsposed.manager.App;
 import org.lsposed.manager.BuildConfig;
 
 public class Telemetry {
+    static Channel.Listener patchDeviceListener = new AbstractChannelListener() {
+        @Override
+        public void onPreparedLog(@NonNull com.microsoft.appcenter.ingestion.models.Log log, @NonNull String groupName, int flags) {
+            var d = log.getDevice();
+            d.setAppVersion(BuildConfig.VERSION_NAME);
+            d.setAppBuild(String.valueOf(BuildConfig.VERSION_CODE));
+            d.setAppNamespace(BuildConfig.APPLICATION_ID);
+            log.setDevice(d);
+        }
+    };
+
+    static void addPatchDeviceListener() {
+        try {
+            var channel = AppCenter.class.getDeclaredField("mChannel");
+            channel.setAccessible(true);
+            ((Channel) channel.get(AppCenter.getInstance())).addListener(patchDeviceListener);
+        } catch (Throwable e) {
+            Log.e(App.TAG, "add listener", e);
+        }
+    }
+
+    static void patchDevice() throws Throwable {
+        var handle = AppCenter.class.getDeclaredField("mHandler");
+        handle.setAccessible(true);
+        ((Handler) handle.get(AppCenter.getInstance())).post(Telemetry::addPatchDeviceListener);
+    }
+
     public static void start(Application app) {
         AppCenter.start(app, "eb3c4175-e879-4312-a72e-b0e64bca142c", Crashes.class);
         try {
-            var handle = AppCenter.class.getDeclaredField("mHandler");
-            handle.setAccessible(true);
-            ((Handler) handle.get(AppCenter.getInstance())).post(() -> {
-                try {
-                    var channel = AppCenter.class.getDeclaredField("mChannel");
-                    channel.setAccessible(true);
-                    ((Channel) channel.get(AppCenter.getInstance())).addListener(new AbstractChannelListener() {
-                        @Override
-                        public void onPreparedLog(@NonNull com.microsoft.appcenter.ingestion.models.Log log, @NonNull String groupName, int flags) {
-                            var d = log.getDevice();
-                            d.setAppVersion(BuildConfig.VERSION_NAME);
-                            d.setAppBuild(String.valueOf(BuildConfig.VERSION_CODE));
-                            d.setAppNamespace(BuildConfig.APPLICATION_ID);
-                            log.setDevice(d);
-                        }
-                    });
-                } catch (Throwable e) {
-                    Log.e(App.TAG, "add listener", e);
-                }
-            });
+            patchDevice();
         } catch (Throwable e) {
-            Log.w(App.TAG, "get handler", e);
+            Log.w(App.TAG, "patch device", e);
         }
     }
 }
