223fdb5c102ce90bbb0686af8387cb9675ee2c01
南宫雪珊 <vvb2060@gmail.com>
2022-01-29 15:36:29 +0800

    Update telemetry (#1578)
    * Update telemetry
    
    * 1

--- app/src/debug/java/org/lsposed/manager/util/Telemetry.java
+++ app/src/debug/java/org/lsposed/manager/util/Telemetry.java
@@ -2,52 +2,66 @@ package org.lsposed.manager.util;
 
 import android.app.Application;
 import android.os.Handler;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.channel.AbstractChannelListener;
 import com.microsoft.appcenter.channel.Channel;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.ingestion.models.Log;
 
 import org.lsposed.manager.App;
 import org.lsposed.manager.BuildConfig;
 
+import java.util.Map;
+
 public class Telemetry {
-    static Channel.Listener patchDeviceListener = new AbstractChannelListener() {
+    private static final Channel.Listener patchDeviceListener = new AbstractChannelListener() {
         @Override
-        public void onPreparedLog(@NonNull com.microsoft.appcenter.ingestion.models.Log log, @NonNull String groupName, int flags) {
-            var d = log.getDevice();
-            d.setAppVersion(BuildConfig.VERSION_NAME);
-            d.setAppBuild(String.valueOf(BuildConfig.VERSION_CODE));
-            d.setAppNamespace(BuildConfig.APPLICATION_ID);
-            log.setDevice(d);
+        public void onPreparedLog(@NonNull Log log, @NonNull String groupName, int flags) {
+            var device = log.getDevice();
+            device.setAppVersion(BuildConfig.VERSION_NAME);
+            device.setAppBuild(String.valueOf(BuildConfig.VERSION_CODE));
         }
     };
 
-    static void addPatchDeviceListener() {
+    private static void addPatchDeviceListener() {
         try {
-            var channel = AppCenter.class.getDeclaredField("mChannel");
-            channel.setAccessible(true);
-            ((Channel) channel.get(AppCenter.getInstance())).addListener(patchDeviceListener);
-        } catch (Throwable e) {
-            Log.e(App.TAG, "add listener", e);
+            var channelField = AppCenter.class.getDeclaredField("mChannel");
+            channelField.setAccessible(true);
+            var channel = (Channel) channelField.get(AppCenter.getInstance());
+            assert channel != null;
+            channel.addListener(patchDeviceListener);
+        } catch (ReflectiveOperationException e) {
+            android.util.Log.e(App.TAG, "add listener", e);
         }
     }
 
-    static void patchDevice() throws Throwable {
-        var handle = AppCenter.class.getDeclaredField("mHandler");
-        handle.setAccessible(true);
-        ((Handler) handle.get(AppCenter.getInstance())).post(Telemetry::addPatchDeviceListener);
+    private static void patchDevice() {
+        try {
+            var handlerField = AppCenter.class.getDeclaredField("mHandler");
+            handlerField.setAccessible(true);
+            var handler = ((Handler) handlerField.get(AppCenter.getInstance()));
+            assert handler != null;
+            handler.post(Telemetry::addPatchDeviceListener);
+        } catch (ReflectiveOperationException e) {
+            android.util.Log.e(App.TAG, "patch device", e);
+        }
     }
 
     public static void start(Application app) {
-        AppCenter.start(app, "eb3c4175-e879-4312-a72e-b0e64bca142c", Crashes.class);
-        try {
-            patchDevice();
-        } catch (Throwable e) {
-            Log.w(App.TAG, "patch device", e);
-        }
+        AppCenter.start(app, "eb3c4175-e879-4312-a72e-b0e64bca142c",
+                Analytics.class, Crashes.class);
+        patchDevice();
+    }
+
+    public static void trackEvent(String name, Map<String, String> properties) {
+        Analytics.trackEvent(name, properties);
+    }
+
+    public static void trackError(Throwable throwable, Map<String, String> properties) {
+        Crashes.trackError(throwable, properties, null);
     }
 }
--- app/src/release/java/org/lsposed/manager/util/Telemetry.java
+++ app/src/release/java/org/lsposed/manager/util/Telemetry.java
@@ -2,6 +2,15 @@ package org.lsposed.manager.util;
 
 import android.app.Application;
 
+import java.util.Map;
+
 public class Telemetry {
-    public static void start(Application application) {}
+    public static void start(Application application) {
+    }
+
+    public static void trackEvent(String name, Map<String, String> properties) {
+    }
+
+    public static void trackError(Throwable throwable, Map<String, String> properties) {
+    }
 }
