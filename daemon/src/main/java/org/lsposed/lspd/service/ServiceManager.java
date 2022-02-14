b7f07dfb7c480e7f819482a9f0777fcbbb607a67
LoveSy <shana@zju.edu.cn>
2022-02-14 20:23:04 +0800

    2

--- daemon/src/main/java/org/lsposed/lspd/service/ServiceManager.java
+++ daemon/src/main/java/org/lsposed/lspd/service/ServiceManager.java
@@ -109,13 +109,14 @@ public class ServiceManager {
 
         systemServerService.putBinderForSystemServer();
 
-        ActivityThread.systemMain();
         DdmHandleAppName.setAppName("lspd", 0);
 
         // get config before package service is started
         // otherwise getInstance will trigger module/scope cache
         var configManager = ConfigManager.getInstance();
         // --- DO NOT call ConfigManager.getInstance later!!! ---
+
+        ActivityThread.systemMain();
 
         waitSystemService("package");
         waitSystemService("activity");
