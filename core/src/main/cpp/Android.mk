d579c895344becee4ec27f720bedc500c3c879a4
LoveSy <shana@zju.edu.cn>
2022-03-15 03:25:30 +0800

    Remove old sources

--- core/src/main/cpp/Android.mk
+++
@@ -1,7 +0,0 @@
-include src/main/jni/external/DexBuilder/Android.mk
-include src/main/jni/external/yahfa/Android.mk
-include src/main/jni/main/Android.mk
-
-$(call import-module,prefab/cxx)
-$(call import-module,prefab/riru)
-$(call import-module,prefab/dobby)
