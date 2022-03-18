0b3f1ea13c6089658a5e292b9d7791b17d3a88c3
LoveSy <shana@zju.edu.cn>
2022-03-18 08:39:54 +0800

    Log `LSPlant`

--- daemon/src/main/jni/logcat.cpp
+++ daemon/src/main/jni/logcat.cpp
@@ -226,7 +226,7 @@ void Logcat::ProcessBuffer(struct log_msg *buf) {
     if (verbose_ && (shortcut || buf->id() == log_id::LOG_ID_CRASH ||
                      entry.pid == my_pid_ || tag == "Magisk"sv || tag == "Dobby"sv ||
                      tag.starts_with("Riru"sv) || tag.starts_with("zygisk"sv) ||
-                     tag.starts_with("LSPosed"sv))) [[unlikely]] {
+                     tag == "LSPlant"sv || tag.starts_with("LSPosed"sv))) [[unlikely]] {
         verbose_print_count_ += PrintLogLine(entry, verbose_file_.get());
     }
     if (entry.pid == my_pid_ && tag == "LSPosedLogcat"sv) [[unlikely]] {
