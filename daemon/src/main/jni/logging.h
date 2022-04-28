21ccbc72520f524a3fba9d65ada8cffae84b1348
LoveSy <shana@zju.edu.cn>
2022-04-28 11:09:45 +0800

    Fix hooking internal used methods

--- daemon/src/main/jni/logging.h
+++ daemon/src/main/jni/logging.h
@@ -28,18 +28,18 @@
 #endif
 
 #ifdef LOG_DISABLED
-#define LOGD(...)
-#define LOGV(...)
-#define LOGI(...)
-#define LOGW(...)
-#define LOGE(...)
+#define LOGD(...) 0
+#define LOGV(...) 0
+#define LOGI(...) 0
+#define LOGW(...) 0
+#define LOGE(...) 0
 #else
 #ifndef NDEBUG
 #define LOGD(fmt, ...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "%s:%d#%s" ": " fmt, __FILE_NAME__, __LINE__, __PRETTY_FUNCTION__ __VA_OPT__(,) __VA_ARGS__)
 #define LOGV(fmt, ...) __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, "%s:%d#%s" ": " fmt, __FILE_NAME__, __LINE__, __PRETTY_FUNCTION__ __VA_OPT__(,) __VA_ARGS__)
 #else
-#define LOGD(...)
-#define LOGV(...)
+#define LOGD(...) 0
+#define LOGV(...) 0
 #endif
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGW(...)  __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
