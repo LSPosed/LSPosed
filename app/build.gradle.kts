b96477e2ea477dfe7c5da707bede2b4c92c3f470
LoveSy <shana@zju.edu.cn>
2022-06-30 16:07:44 +0800

    No mainswitchbar 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -164,7 +164,7 @@ dependencies {
     implementation("dev.rikka.rikkax.preference:simplemenu-preference:1.0.3")
     implementation("dev.rikka.rikkax.recyclerview:recyclerview-ktx:1.3.1")
     implementation("dev.rikka.rikkax.widget:borderview:1.1.0")
-    implementation("dev.rikka.rikkax.widget:mainswitchbar:1.1.0")
+    implementation("dev.rikka.rikkax.widget:mainswitchbar:1.0.2")
     implementation("dev.rikka.rikkax.layoutinflater:layoutinflater:1.2.0")
     implementation("me.zhanghai.android.appiconloader:appiconloader:1.4.0")
     implementation("org.lsposed.hiddenapibypass:hiddenapibypass:4.3")
