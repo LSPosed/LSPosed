b9208bb238a73954959e3aec1b1dd739d5bfdfce
LoveSy <shana@zju.edu.cn>
2022-07-10 22:49:56 +0800

    Update context.h

--- core/src/main/jni/include/context.h
+++ core/src/main/jni/include/context.h
@@ -109,7 +109,7 @@ namespace lspd {
             }
             jmethodID mid = lsplant::JNI_GetStaticMethodID(env, entry_class_, method_name, method_sig);
             if (mid) [[likely]] {
-                env->CallStaticVoidMethod(entry_class_, mid, std::forward<Args>(args)...);
+                env->CallStaticVoidMethod(entry_class_, mid, lsplant::UnwrapScope(std::forward<Args>(args))...);
             } else {
                 LOGE("method {} id is null", method_name);
             }
