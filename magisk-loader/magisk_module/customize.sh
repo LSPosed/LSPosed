260a5e11702896a8c3762a61510c3ef4c3b55d51
残页 <31466456+canyie@users.noreply.github.com>
2022-07-25 07:37:10 +0800

    Remove extraction of libdaemon.so (#2050)

--- magisk-loader/magisk_module/customize.sh
+++ magisk-loader/magisk_module/customize.sh
@@ -88,16 +88,6 @@ extract "$ZIPFILE" 'daemon'             "$MODPATH"
 rm -f /data/adb/lspd/manager.apk
 extract "$ZIPFILE" 'manager.apk'        '/data/adb/lspd'
 
-ui_print "- Extracting daemon libraries"
-if [ "$ARCH" = "arm" ] ; then
-  extract "$ZIPFILE" 'lib/armeabi-v7a/libdaemon.so' "$MODPATH" true
-elif [ "$ARCH" = "arm64" ]; then
-  extract "$ZIPFILE" 'lib/arm64-v8a/libdaemon.so' "$MODPATH" true
-elif [ "$ARCH" = "x86" ]; then
-  extract "$ZIPFILE" 'lib/x86/libdaemon.so' "$MODPATH" true
-elif [ "$ARCH" = "x64" ]; then
-  extract "$ZIPFILE" 'lib/x86_64/libdaemon.so' "$MODPATH" true
-fi
 if [ "$FLAVOR" == "zygisk" ]; then
   mkdir -p "$MODPATH/zygisk"
   if [ "$ARCH" = "arm" ] || [ "$ARCH" = "arm64" ]; then
