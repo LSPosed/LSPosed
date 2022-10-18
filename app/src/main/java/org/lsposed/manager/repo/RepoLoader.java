333fb3189bcf21e0feffdcf9b0ae0c4d82b91d6c
Howard Wu <40033067+Howard20181@users.noreply.github.com>
2022-10-18 12:40:05 +0800

    Update RepoLoader.java

--- app/src/main/java/org/lsposed/manager/repo/RepoLoader.java
+++ app/src/main/java/org/lsposed/manager/repo/RepoLoader.java
@@ -35,7 +35,6 @@ import org.lsposed.manager.repo.model.Release;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
