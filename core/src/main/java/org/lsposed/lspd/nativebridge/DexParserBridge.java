39bd97adab645bc8595262b2a8ad72d44e746a20
LoveSy <shana@zju.edu.cn>
2023-01-07 02:25:04 +0800

    Make parseMethod fastnative

--- core/src/main/java/org/lsposed/lspd/nativebridge/DexParserBridge.java
+++ core/src/main/java/org/lsposed/lspd/nativebridge/DexParserBridge.java
@@ -3,8 +3,11 @@ package org.lsposed.lspd.nativebridge;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+import dalvik.annotation.optimization.FastNative;
+
 public class DexParserBridge {
     public static native Object parseDex(ByteBuffer dex) throws IOException;
 
+    @FastNative
     public static native Object parseMethod(ByteBuffer dex, int codeOffset);
 }
