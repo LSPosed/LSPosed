d826aa2b9ec63ada652ed51f0ed599d9cb7fdd95
forkWo <119728345+forkWo@users.noreply.github.com>
2023-05-01 12:42:01 +0800

    Update build.gradle.kts

--- magisk-loader/build.gradle.kts
+++ magisk-loader/build.gradle.kts
@@ -159,7 +159,7 @@ fun afterEval() = android.applicationVariants.forEach { variant ->
                 "versionName" to "v$verName",
                 "versionCode" to verCode,
                 "authorList" to authors,
-                "updateJson" to "https://lsposed.github.io/LSPosed/release/${flavorLowered}.json",
+                "updateJson" to "https://github.com/forkWo/LSPosed/release/${flavorLowered}.json",
                 "requirement" to when (flavorLowered) {
                     "riru" -> "Requires Riru $moduleMinRiruVersionName or above installed"
                     "zygisk" -> "Requires Magisk 24.0+ and Zygisk enabled"
