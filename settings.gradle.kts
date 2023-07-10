7fe84388e4d29310be38c1809d65d86ebf2b8a23
LoveSy <shana@zju.edu.cn>
2023-07-10 21:46:42 +0800

    Upgrade gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
     repositories {
         google()
         mavenCentral()
