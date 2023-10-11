d4a41625b7465f6bd0613f99194a44c14a3db448
LoveSy <shana@zju.edu.cn>
2023-10-11 08:57:06 +0800

    Add tag `LSPosedContext` to module log

--- daemon/src/main/jni/logcat.cpp
+++ daemon/src/main/jni/logcat.cpp
@@ -220,7 +220,7 @@ void Logcat::ProcessBuffer(struct log_msg *buf) {
 
     std::string_view tag(entry.tag, entry.tagLen);
     bool shortcut = false;
-    if (tag == "LSPosed-Bridge"sv || tag == "XSharedPreferences"sv) [[unlikely]] {
+    if (tag == "LSPosed-Bridge"sv || tag == "XSharedPreferences"sv || tag == "LSPosedContext") [[unlikely]] {
         modules_print_count_ += PrintLogLine(entry, modules_file_.get());
         shortcut = true;
     }
