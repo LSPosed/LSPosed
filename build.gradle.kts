87204c53526871f5d7fe002de1bf2fa1e246d7e8
vvb2060 <vvb2060@gmail.com>
2024-01-07 12:48:00 +0800

    update ndk

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,7 +70,7 @@ val androidTargetSdkVersion by extra(34)
 val androidMinSdkVersion by extra(27)
 val androidBuildToolsVersion by extra("34.0.0")
 val androidCompileSdkVersion by extra(34)
-val androidCompileNdkVersion by extra("26.0.10792818")
+val androidCompileNdkVersion by extra("26.1.10909125")
 val androidSourceCompatibility by extra(JavaVersion.VERSION_17)
 val androidTargetCompatibility by extra(JavaVersion.VERSION_17)
 
