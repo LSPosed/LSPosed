f286194e21b1e32aa3f9aca458c54cb0677475b9
Caner Karaca <37447503+CanerKaraca23@users.noreply.github.com>
2024-01-09 21:36:16 +0300

    Update build.gradle.kts

--- hiddenapi/stubs/build.gradle.kts
+++ hiddenapi/stubs/build.gradle.kts
@@ -22,6 +22,6 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
